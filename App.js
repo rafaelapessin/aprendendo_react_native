fad3b2c17317bc353267bf26e3aa95fe8744ce9d
rafaelaapessin <97127098+rafaelaapessin@users.noreply.github.com>
2025-10-24 17:41:29 -0300

    Objetos e FlatList

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import {StyleSheet, View, FlatList} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import "react-native-safe-area-context";
 
@@ -15,9 +15,10 @@ export default function App() {
       <SafeAreaView style={styles.container}>
         <Topo/>
         <View style={styles.supermercados}>
-          <Supermercado imagem={carrefour} nome="Carrefour" distancia="1200m"/>
-          <Supermercado imagem={bh} nome="BH" distancia="800m"/>
-          <Supermercado imagem={extra} nome="Extra" distancia="2400m"/>
+          <FlatList 
+          data={supermercados.lista} 
+          renderItem={({item}) => <Supermercado {...item}/>}
+          />
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -35,3 +36,28 @@ const styles = StyleSheet.create({
   },
 });
 
+const gerarNumAleatorio = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const supermercados ={
+  titulo: 'Supermercados',
+  // lista é um array de objetos
+  lista: [
+    {
+      imagem: carrefour,
+      nome: 'Carrefour',
+      distancia: gerarNumAleatorio(1, 1500) + 'metros',
+    },
+    {
+      imagem: bh,
+      nome: 'BH',
+      distancia: gerarNumAleatorio(1, 1500) + 'metros',
+    },
+    {
+      imagem: extra,
+      nome: 'Extra',
+      distancia: gerarNumAleatorio(1, 1500) + 'metros',
+    }
+  ]
+}
